899e8ba1523388621b66b69d7a56aa9b04b1daf1
Felipe Vianna <felipe.vianna86@gmail.com>
2024-04-08 19:13:49 -0300

    first commit

---
+++ src/main/java/com/example/javaaipowered/JavaAiPoweredApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaaipowered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAiPoweredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAiPoweredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaaipowered/JavaAiPoweredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaaipowered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAiPoweredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
